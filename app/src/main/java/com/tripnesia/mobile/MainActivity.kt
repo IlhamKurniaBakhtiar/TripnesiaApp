e4f949218c49be93e897eb5a70a44b1be259be84
Ilham Kurnia Bakhtiar <ilhamkurniabakhtiar@gmail.com>
2025-05-28 22:59:17 +0700

    Menambahkan animasi saat memasuki aplikasi (fade out)

--- app/src/main/java/com/tripnesia/mobile/MainActivity.kt
+++ app/src/main/java/com/tripnesia/mobile/MainActivity.kt
@@ -7,19 +7,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
 import com.tripnesia.mobile.ui.TripnesiaMainScreen
+import com.tripnesia.mobile.ui.SplashScreen
 import com.tripnesia.mobile.ui.theme.TripnesiaAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TripnesiaAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TripnesiaMainScreen()
+                    var isSplashScreenVisible by remember { mutableStateOf(true) }
+
+                    // Menampilkan SplashScreen dulu, kemudian pindah ke MainScreen
+                    if (isSplashScreenVisible) {
+                        SplashScreen(onAnimationEnd = {
+                            isSplashScreenVisible = false
+                        })
+                    } else {
+                        TripnesiaMainScreen()
+                    }
                 }
             }
         }
